1a46427e5b4a4b695d60723d51f370870be43e93
Carlos Martinez <carlos.m.ch03@gmail.com>
2021-02-02 13:04:48 +0100

    Creamos las clases Pieza y Serpiente.

---
+++ core/src/com/snake/game/Serpiente.java
@@ -0,0 +1,37 @@
+package com.snake.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.ArrayList;
+
+public class Serpiente {
+    private ArrayList<Pieza> Serpiente = new ArrayList();
+    private Pieza piezaSerpiente;
+    private int posX;
+    private int posY;
+    private Texture imgSerpiente;
+    private String file_serpiente = "serpiente.png";
+
+    public Serpiente(int PosX, int PosY, int nancho) {
+        this.Serpiente.add(new Pieza(PosX, PosY, nancho));
+        this.imgSerpiente = new Texture(this.file_serpiente);
+    }
+
+    public void pintate(SpriteBatch miSB) {
+        TextureRegion serpiente = new TextureRegion(this.imgSerpiente, this.posX, this.posY);
+        miSB.begin();
+        miSB.draw(serpiente, 0.0F, 0.0F);
+        miSB.end();
+    }
+
+    public void moverse() {
+    }
+
+    public void crecer() {
+    }
+
+    public void dispose() {
+    }
+}
